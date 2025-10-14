978d8342b0b9aa1bb9e47e4e225ee39750eed4a1
Harshit004 <rajharshit04@gmail.com>
2025-10-14 14:16:57 +0530

    Updated gap between last section and footer on careers 3

--- app/careers3/page.tsx
+++ app/careers3/page.tsx
@@ -499,7 +499,7 @@ export default function Careers2() {
     </section>
 
     {/* Explore our Resources Section */}
-    <section className="w-full px-[9.722222%] max-w-[1440px] mx-auto mt-[140px]">
+    <section className="w-full px-[9.722222%] max-w-[1440px] mx-auto mt-[140px] pb-[200px]">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-x-16 justify-between">
         <div style={{
           fontFamily: '"Inter Tight", sans-serif',
