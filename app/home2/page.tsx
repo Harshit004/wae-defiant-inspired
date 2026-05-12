33b378d1da88d160b8ae81fee636bb521e7a3fc9
Harshit004 <rajharshit04@gmail.com>
2026-05-13 00:42:28 +0530

    Move Products section horizontal rule to the very bottom of the section

--- app/home2/page.tsx
+++ app/home2/page.tsx
@@ -641,7 +641,7 @@ export default function Home() {
 
             {/* Our Products Section */}
             <section
-                className="bg-black text-white"
+                className="bg-black text-white border-b border-white"
                 style={{
                     paddingTop: '124px',
                     paddingBottom: '124px',
@@ -698,7 +698,7 @@ export default function Home() {
 
                     <div style={{ height: '18px' }} />
 
-                    <div className="grid grid-cols-4 w-full border-b border-white" style={{ height: '506px' }}>
+                    <div className="grid grid-cols-4 w-full" style={{ height: '506px' }}>
                         {/* Aurela */}
                         <div className="relative group cursor-pointer h-full overflow-hidden">
                             <Image
