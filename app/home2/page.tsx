ee885856201fb000f73b144bd90da13465054d14
Harshit004 <rajharshit04@gmail.com>
2026-05-20 18:01:46 +0530

    fix: mt in carbon neutral section

--- app/home2/page.tsx
+++ app/home2/page.tsx
@@ -905,7 +905,7 @@ export default function Home() {
                             </p>
                         </div>
 
-                        <div className="mt-[190px]">
+                        <div className="mt-[200px]">
                             <motion.div
                                 initial={{ filter: 'grayscale(100%)' }}
                                 whileHover={{ filter: 'grayscale(0%)' }}
