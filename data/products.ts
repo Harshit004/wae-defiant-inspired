51f30b3c10df26c10c33d3a72999988833a9faa5
HARSHIT <69170819+Harshit004@users.noreply.github.com>
2026-06-25 17:33:31 +0530

    cms: update products database

--- data/products.ts
+++ data/products.ts
@@ -81,10 +81,10 @@ export const CATEGORIES: Record<string, CategoryData> = {
     ],
     "products": [
       {
-        "id": "gsp",
-        "name": "GSP",
-        "category": "free-standing",
-        "image": "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/062c00fb-5b0a-4167-2d2b-120af5406e00/public"
+        "id": "eno-ct",
+        "name": "ENKI.CT",
+        "category": "counter-top",
+        "image": "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/fbf516d7-077e-47f3-6aed-895881ab0c00/public"
       },
       {
         "id": "var-ct",
@@ -141,10 +141,10 @@ export const CATEGORIES: Record<string, CategoryData> = {
         "image": "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/969cf7aa-9a0c-4e02-c897-8ec75b1b5a00/public"
       },
       {
-        "id": "eno-ct",
-        "name": "ENKI.CT",
-        "category": "counter-top",
-        "image": "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/fbf516d7-077e-47f3-6aed-895881ab0c00/public"
+        "id": "gsp",
+        "name": "GSP",
+        "category": "free-standing",
+        "image": "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/062c00fb-5b0a-4167-2d2b-120af5406e00/public"
       }
     ],
     "imageUrl": "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/9dac2dbe-676f-4f2b-b09f-cc64cd253700/public"
@@ -503,13 +503,13 @@ export const PRODUCTS: Record<string, ProductDetails> = {
     },
     "status": "Live",
     "description": "For instance, a 50 LPH machine operating continuously for 8 hours will produce 400 liters of\nwater daily, amounting to 8,800 liters over a month.",
-    "heroImage": "",
+    "heroImage": "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/f548679e-2ba4-4968-e281-2fde42242800/public",
     "heroTagline": "Plastic in passe, Landfilling is Zero.\nSustainability is the Future.",
     "heroSubtext": "Sustainability at Core",
     "heroCtaText": "Contact Us",
-    "heroCtaLink": "",
+    "heroCtaLink": "/Contactus",
     "showcaseCtaText": "Enquire Now",
-    "showcaseCtaLink": "",
+    "showcaseCtaLink": "/enquiry",
     "brochurePdf": "file:///C:/Users/WAE/Downloads/ENKI.pdf",
     "variants": {
       "hot": true,
@@ -800,7 +800,7 @@ export const PRODUCTS: Record<string, ProductDetails> = {
     "id": "gsp",
     "name": "GSP",
     "categoryName": "DRINKING WATER STATION - BLUWAE Series",
-    "heroSubtitle": "Powerful LED sterilization",
+    "heroSubtitle": "",
     "images": [
       "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/062c00fb-5b0a-4167-2d2b-120af5406e00/public",
       "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/94110d2a-1b49-4225-cd63-a1bb1bcfc300/public",
@@ -836,7 +836,7 @@ export const PRODUCTS: Record<string, ProductDetails> = {
     "specifications": {
       "storageCapacity": [
         {
-          "variant": "GSP HC",
+          "variant": "GSP FS",
           "hot": "1",
           "cold": "16",
           "ambient": "-"
@@ -851,7 +851,7 @@ export const PRODUCTS: Record<string, ProductDetails> = {
       "refrigerant": "R-134a",
       "dimensions": [
         {
-          "variant": "GSP HC",
+          "variant": "GSP FS",
           "weight": "131.0",
           "height": "1155",
           "width": "300",
@@ -864,13 +864,13 @@ export const PRODUCTS: Record<string, ProductDetails> = {
     },
     "status": "Live",
     "description": "For instance, a 50 LPH machine operating continuously for 8 hours will produce 400 liters of\nwater daily, amounting to 8,800 liters over a month.",
-    "heroImage": "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/ad026cb7-dac1-4188-a839-a2062c712d00/public",
+    "heroImage": "",
     "heroTagline": "Plastic in passe, Landfilling is Zero.\nSustainability is the Future.",
     "heroSubtext": "Sustainability at Core",
     "heroCtaText": "Contact Us",
-    "heroCtaLink": "/contactus",
+    "heroCtaLink": "",
     "showcaseCtaText": "Enquire Now",
-    "showcaseCtaLink": "/enqyiry",
+    "showcaseCtaLink": "",
     "brochurePdf": "file:///C:/Users/WAE/Downloads/Grand%20Slam%20Pro.pdf",
     "variants": {
       "hot": true,
@@ -1077,7 +1077,7 @@ export const PRODUCTS: Record<string, ProductDetails> = {
     "id": "eno-ct",
     "name": "ENKI.CT",
     "categoryName": "DRINKING WATER STATION - BLUWAE Series",
-    "heroSubtitle": "",
+    "heroSubtitle": "Powerful LED sterilization",
     "images": [
       "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/fbf516d7-077e-47f3-6aed-895881ab0c00/public",
       "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/836e8089-a92c-4171-9060-f61c95591700/public",
@@ -1149,13 +1149,13 @@ export const PRODUCTS: Record<string, ProductDetails> = {
     },
     "status": "Live",
     "description": "For instance, a 50 LPH machine operating continuously for 8 hours will produce 400 liters of\nwater daily, amounting to 8,800 liters over a month.",
-    "heroImage": "",
-    "heroTagline": "Plastic is passe, Landfilling is zero.\nSustainability is the future.",
+    "heroImage": "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/9165c682-a912-4247-c5f9-ce3ffaed8700/public",
+    "heroTagline": "For instance, a 50 LPH machine operating continuously for 8 hours will produce 400 liters of\nwater daily, amounting to 8,800 liters over a month.",
     "heroSubtext": "Sustainability at Core",
     "heroCtaText": "Contact Us",
-    "heroCtaLink": "",
+    "heroCtaLink": "/contactus",
     "showcaseCtaText": "Enquire Now",
-    "showcaseCtaLink": "",
+    "showcaseCtaLink": "/enquiry",
     "brochurePdf": "file:///C:/Users/WAE/Downloads/ENKI.pdf",
     "variants": {
       "hot": true,
