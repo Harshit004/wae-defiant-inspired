778b5dddfbda4b33517f64f9d733bf2feba16d6c
HARSHIT <69170819+Harshit004@users.noreply.github.com>
2026-06-25 16:13:12 +0530

    cms: update subscribers database

--- data/subscribers.ts
+++ data/subscribers.ts
@@ -6,4 +6,12 @@ export interface Subscriber {
   pageLink: string;
 }
 
-export const SUBSCRIBERS: Subscriber[] = [];
+export const SUBSCRIBERS: Subscriber[] = [
+  {
+    "id": "aa36bc36-1a75-4ed7-a7ae-c0a0e6b492d8",
+    "email": "test@testmail.com",
+    "date": "2026-06-25",
+    "time": "10:43:12",
+    "pageLink": "/join-wae/product-designer"
+  }
+];
