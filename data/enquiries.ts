aa7f448f127205f67f077205c0a5e695586074e6
HARSHIT <69170819+Harshit004@users.noreply.github.com>
2026-06-28 20:05:21 +0530

    cms: update enquiries database

--- data/enquiries.ts
+++ data/enquiries.ts
@@ -8,4 +8,14 @@ export interface Enquiry {
   createdAt: string;
 }
 
-export const ENQUIRIES: Record<string, Enquiry> = {};
+export const ENQUIRIES: Record<string, Enquiry> = {
+  "3c25a3fd-d4ad-4192-ae72-670f548d050a": {
+    "id": "3c25a3fd-d4ad-4192-ae72-670f548d050a",
+    "fullName": "hi",
+    "companyName": "Test company",
+    "email": "test@mail.com",
+    "phone": "12345 67890",
+    "pageLink": "https://wae-ltd.vercel.app/product-description-page?product=venus",
+    "createdAt": "2026-06-28T14:35:20.948Z"
+  }
+};
