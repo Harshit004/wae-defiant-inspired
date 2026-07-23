8ad7756b47a1f63e689b87bb8257fba76fe4d85b
Harshit004 <rajharshit04@gmail.com>
2026-07-24 03:40:22 +0530

    Resize scroll-to-top button to be exactly 10% smaller than whatsapp widget responsively

--- components/scroll-to-top.tsx
+++ components/scroll-to-top.tsx
@@ -36,15 +36,15 @@ export default function ScrollToTop() {
   return (
     <button
       onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-      className="fixed bottom-[85px] right-5 md:bottom-[100px] md:right-6 z-[9999] hover:opacity-80 transition-opacity flex items-center justify-center"
-      style={{ width: "54px", height: "54px" }}
+      className="fixed bottom-[85px] right-5 md:bottom-[100px] md:right-6 z-[9999] hover:opacity-80 transition-opacity flex items-center justify-center w-[41px] h-[41px] md:w-[49px] md:h-[49px]"
+      
       aria-label="Scroll to top"
     >
       <Image
         src="https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/4a558429-0490-4c44-df30-94e4625a4600/public"
         alt="Scroll to top"
-        width={54}
-        height={54}
+        fill
+        className="object-contain"
       />
     </button>
   )
