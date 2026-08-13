b0b2842eb5bef8878d6c7c10b7bd8fd6e0fea888
HARSHIT <69170819+Harshit004@users.noreply.github.com>
2026-08-13 17:35:29 +0530

    cms: update blogs database

--- data/blogs.ts
+++ data/blogs.ts
@@ -57,7 +57,7 @@ export const WRITERS: Record<string, Writer> = {
     "name": "Shambhavi Yadav",
     "role": "Marketing",
     "bio": "Ms. Shambhavi Yadav,  a national award recipient at Rashtrapati Bhavan, explores sustainability & policy. With over 8 years of experience, she is NET qualified, a graduate of LSR, DU, and a KPMG Scholar.",
-    "image": "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/1e1ad843-5273-45b9-f95b-7e3265805a00/public",
+    "image": "https://imagedelivery.net/R9aLuI8McL_Ccm6jM8FkvA/4e302629-bd9a-4de1-9075-c3c788eb1500/public",
     "link": "https://in.linkedin.com/in/shambhavi-yadav-61b937276"
   }
 };
