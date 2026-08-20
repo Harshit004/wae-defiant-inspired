c836be081116c84ab43958d8955a717721f7ef6f
HARSHIT <69170819+Harshit004@users.noreply.github.com>
2026-08-20 10:51:41 +0530

    cms: update jobs database

--- data/jobs.ts
+++ data/jobs.ts
@@ -31,7 +31,7 @@ export const JOBS: Record<string, JobPost> = {
     "category": "MARKETING",
     "type": "On-site",
     "time": "Full time",
-    "shortDescription": "This role is best suited for someone who enjoys solving problems,..........\n",
+    "shortDescription": "This role is best suited for someone who enjoys solving problems,.... \n",
     "status": "Live",
     "createdAt": "2024-01-01T00:00:00Z",
     "summaryParagraphs": [
